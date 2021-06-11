a9a6b70455b78547288e1ddcfc77692a1315a747
Ivan Andres <ivan_andi@hotmail.com>
2021-06-10 19:26:30 -0500

    'pruebadeseguridad'

--- pages/proyecto1/settings.py
+++ pages/proyecto1/settings.py
@@ -20,9 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-m@=-0vpl3=#nrz5l_fi+l22vcruv@!1=56$(dkeg52g@kmwkv!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = int(os.environ.get('DEBUG',default=0))
 #valida si estamos en productivo
